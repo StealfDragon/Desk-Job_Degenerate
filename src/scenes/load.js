bf399030b54cf1b01abf77b55285696093479eb4
Gloomsfield <amory@acosta.boo>
2026-04-07 01:10:34 -0700

    add debug assets/functionality for outer game

--- src/scenes/load.js
+++ src/scenes/load.js
@@ -3,6 +3,9 @@ class Load extends Phaser.Scene {
 		super("load_scene");
 	}
 
-	preload() {}
+	preload() {
+		this.load.image("button_delete", "assets/ui/button_delete.png");
+		this.load.image("button_respond", "assets/ui/button_respond.png");
+	}
 }
 
--- src/scenes/outer-game.js
+++ src/scenes/outer-game.js
@@ -3,6 +3,26 @@ class OuterGame extends Phaser.Scene {
 		super("outer-game_scene");
 	}
 
-	create() {}
+	create() {
+		this.button_delete = this.add.image(
+			0, 0, "button_delete"
+		).setInteractive().on("pointerdown", () => {
+			this.email_delete();
+		});
+
+		this.button_respond = this.add.image(
+			100, 0, "button_respond"
+		).setInteractive().on("pointerdown", () => {
+			this.email_respond();
+		});
+	}
+
+	email_delete() {
+		console.log("deleted email!");
+	}
+
+	email_respond() {
+		console.log("responded to email!");
+	}
 }
 
