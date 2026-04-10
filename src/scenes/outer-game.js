fee562bd8bf24b30db2b42f0cd33d790cd0bcf95
xNoflo <noflo1101@gmail.com>
2026-04-10 10:21:48 -0700

    adding outer game asset

--- src/scenes/outer-game.js
+++ src/scenes/outer-game.js
@@ -3,6 +3,11 @@ class OuterGame extends Phaser.Scene {
 		super("outer-game_scene");
 	}
 
+	preload() {
+	this.load.image('outertv', 'assets/images/outergametv.png');
+    
+	  }
+
 	create() {}
 }
 
