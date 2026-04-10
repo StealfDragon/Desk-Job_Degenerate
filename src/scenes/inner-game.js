16edfd8286905e2602a165f67b417f13a68e7ba0
xNoflo <noflo1101@gmail.com>
2026-04-10 09:48:45 -0700

    Pushing UI assets/inner game

--- src/scenes/inner-game.js
+++ src/scenes/inner-game.js
@@ -3,6 +3,14 @@ class InnerGame extends Phaser.Scene {
 		super("inner-game_scene");
 	}
 
+	preload() {
+    this.load.image('inventory', 'assets/images/inventory.png');
+    this.load.image('pausemenu', 'assets/images/Play-Options-Exit.png)');
+    this.load.image('levelborder', 'assets/images/Level Border.png');
+    this.load.spritesheet('rogueplayer', 'assets/images/doc.png', { frameWidth: 16, frameHeight: 16 });
+    
+	  }
+
 	create() {}
 }
 
